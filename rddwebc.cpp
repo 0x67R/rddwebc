7900ed5257d9971f8867dbf59bd8e86d12b4ac0e
Rafael Godoy <51085678+0x67R@users.noreply.github.com>
2020-03-04 12:03:35 +0000

    Update. Parts of the code were ported to C++

--- rddwebc.cpp
+++ rddwebc.cpp
@@ -3,21 +3,33 @@ GNU General Public License v3.0
 
 Copyright (C) 2019, Rafael Godoy, 0x67rafael@protonmail.com .
 
-RDDWEBC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+RDDWEBC is free software: you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
 
-RDDWEBC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+RDDWEBC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with RDDWEBC. If not, see https://www.gnu.org/licenses/.
+You should have received a copy of the GNU General Public License along with RDDWEBC. 
+If not, see https://www.gnu.org/licenses/.
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Curl and libcurl are true Open Source/Free Software and meet all definitions as such. It means that you are free to modify and redistribute all contents of the curl distributed archives. You may also freely use curl and libcurl in your commercial projects.
+Curl and libcurl are true Open Source/Free Software and meet all definitions as such. 
+It means that you are free to modify and redistribute all contents of the curl distributed archives. 
+You may also freely use curl and libcurl in your commercial projects.
 
 Curl and libcurl are licensed under the license below, which is inspired by MIT/X, but not identical.
 
 Copyright (C) 1998 - 2015, Daniel Stenberg, daniel@haxx.se, et al.
 
-This software is licensed as described in the file COPYING, which you should have received as part of this distribution. The terms are also available at https://curl.haxx.se/docs/copyright.html. You may opt to use, copy, modify, merge, publish, distribute and/or sell copies of the Software, and permit persons to whom the Software is furnished to do so, under the terms of the COPYING file. This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+This software is licensed as described in the file COPYING, 
+which you should have received as part of this distribution. 
+The terms are also available at https://curl.haxx.se/docs/copyright.html. 
+You may opt to use, copy, modify, merge, publish, distribute and/or sell copies of the Software, 
+and permit persons to whom the Software is furnished to do so, under the terms of the COPYING file. 
+This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 
 */
 
@@ -27,40 +39,37 @@ This software is licensed as described in the file COPYING, which you should hav
 #include <string>
 #include <list>
 #include <fstream>
-#include <string.h>
 #include <cstring>
-#include <time.h>
+#include <ctime>
 #include <unistd.h>
 #include <getopt.h>
 #include <curl/curl.h>
-#include <errno.h>
+#include <cerrno>
 
-
-using namespace std;
 
 typedef signed long int slong_int;
 
 #define ERROR_INTERN_RDDWEBCGET 999;
 
 
-static void METHOD_USAGE_RDDWEBC(void)
+static void method_usage_rddwebc(void)
 {
-    cout << "\n\n[!] Usage: rddwebc [options...]\n\n"
+    std::cout << "\n\n[!] Usage: rddwebc [options...]\n\n"
          "\t-u, --url    \t   Request website\n"
          "\t-p, --path   \t   Enter the wordlist path if you have chosen BRUTEFORCE. If not, type a STRING\n"
          "\t-a, --agent  \t   To use the default agent, set 'default', "
-         "if you want to use one of your choice, set '-a <YOUR USER-AGENT>'\n" << endl;
+         "if you want to use one of your choice, set '-a <YOUR USER-AGENT>'\n" << std::endl;
 }
 
-static void BANNER_RDDWEBC(void)
+static void banner_rddwebc(void)
 {
-    cout << "\n\t========= <RDDWEBC WEB CRAWLING> [FOCUSED ON DIRECTORIES]  =========\n"
+    std::cout << "\n\t========= <RDDWEBC WEB CRAWLING> [FOCUSED ON DIRECTORIES]  =========\n"
             "\tCopyright (C) 2019, Rafael Godoy, <0x67rafael@protonmail.com>\n"
             "\t\t      Github: https://github.com/0x67R\n\n"
-            "\t____________________________________________________________________\n" << endl;
+            "\t____________________________________________________________________\n" << std::endl;
 }
 
-static char *TIME_STAMP(void)
+static std::string time_stamp(void)
 {
     time_t cal_time;
     cal_time = time(NULL);
@@ -68,16 +77,16 @@ static char *TIME_STAMP(void)
     return act_time_stamp;
 }
 
-static list<string> WORDLIST(char const *path)
+static std::list<std::string> wordlist(const std::string &path)
 {
-    ifstream path_reading;
+    std::ifstream path_reading;
     path_reading.open(path);
 
-    list<string> word_list;
-    string buffer_tmp;
+    std::list<std::string> word_list;
+    std::string buffer_tmp;
 
     if (path_reading.is_open()) {
-        while (getline(path_reading, buffer_tmp))
+        while (std::getline(path_reading, buffer_tmp))
             word_list.push_back(buffer_tmp);
         buffer_tmp.clear();
         path_reading.close();
@@ -87,18 +96,20 @@ static list<string> WORDLIST(char const *path)
         path_reading.close();
         return word_list;
     } else {
-        cerr << "[!] PARAMETER ENTERED FOR OPTION [PATH] IS INVALID\n[!]WARNING, ABORTING SOFTWARE" << endl;
+        std::cerr << "[!] PARAMETER ENTERED FOR OPTION [PATH] IS INVALID\n"
+                     "[!] WARNING, ABORTING SOFTWARE" << std::endl;
+
         exit(EXIT_FAILURE);
     }
 }
 
 
-size_t NOT_VERBOSE(void *buffer, size_t size, size_t nmemb, void *userp)
+static size_t not_verbose(void *buffer, size_t size, size_t nmemb, void *userp)
 {
     return size * nmemb;
 }
 
-static slong_int RDDWEBC_GET(char const *url, char const *user_agent)
+static slong_int rddwebc_get(const std::string &url, const std::string &user_agent)
 {
     CURL *rddwebc;
     CURLcode response_code;
@@ -109,10 +120,10 @@ static slong_int RDDWEBC_GET(char const *url, char const *user_agent)
     rddwebc = curl_easy_init();
 
     if (rddwebc) {
-        curl_easy_setopt(rddwebc, CURLOPT_USERAGENT, user_agent);
-        curl_easy_setopt(rddwebc, CURLOPT_URL, url);
+        curl_easy_setopt(rddwebc, CURLOPT_USERAGENT, user_agent.c_str());
+        curl_easy_setopt(rddwebc, CURLOPT_URL, url.c_str());
         curl_easy_setopt(rddwebc, CURLOPT_NOBODY, 1L);
-        curl_easy_setopt(rddwebc, CURLOPT_WRITEFUNCTION, NOT_VERBOSE);
+        curl_easy_setopt(rddwebc, CURLOPT_WRITEFUNCTION, not_verbose);
 
         #ifdef SKIP_PEER_VERIFICATION
 
@@ -147,7 +158,7 @@ static slong_int RDDWEBC_GET(char const *url, char const *user_agent)
         response_code = curl_easy_perform(rddwebc);
 
         if (response_code != CURLE_OK) {
-            cerr << "[!!!] WARNING, RDDWEBC ERROR: " << curl_easy_strerror(response_code) << endl;
+            std::cerr << "[!!!] WARNING, RDDWEBC ERROR: " << curl_easy_strerror(response_code) << std::endl;
             curl_easy_cleanup(rddwebc);
             curl_global_cleanup();
             return ERROR_INTERN_RDDWEBCGET;
@@ -158,7 +169,7 @@ static slong_int RDDWEBC_GET(char const *url, char const *user_agent)
         curl_easy_cleanup(rddwebc);
 
     } else {
-        cerr << "[!!!] FATAL ERROR: " << strerror(errno) << endl;
+        std::cerr << "[!!!] FATAL ERROR: " << strerror(errno) << std::endl;
         curl_easy_cleanup(rddwebc);
         curl_global_cleanup();
         exit(EXIT_FAILURE);
@@ -167,35 +178,37 @@ static slong_int RDDWEBC_GET(char const *url, char const *user_agent)
     curl_global_cleanup();
 
     return http_responde_code;
-}
+};
 
-static void ___RDDWEBCRAWLING___(char const *url, list<string> brut_wordlist, string agent)
+static void ___rddwebcrawling___(const std::string &url, const std::list<std::string> &brut_wordlist, std::string agent)
 {
     if (agent.empty() || agent == "default")
         agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
 
-    BANNER_RDDWEBC();
+    banner_rddwebc();
 
-    cout << "[*] INITIATING THE REQUISITIONS..\n" << endl;
+    std::cout << "[*] INITIATING THE REQUISITIONS..\n" << std::endl;
 
-    for (list<string>::iterator brut_wordlist_it = brut_wordlist.begin(); brut_wordlist_it != brut_wordlist.end(); brut_wordlist_it++) {
+    std::list<std::string>::const_iterator brut_wordlist_it;
 
-        string url_crawling = url;
+    for (brut_wordlist_it = brut_wordlist.begin(); brut_wordlist_it != brut_wordlist.end(); brut_wordlist_it++) {
+
+        std::string url_crawling = url;
         url_crawling.append(("/" + *brut_wordlist_it));
-        slong_int rddwebc_get_return = RDDWEBC_GET(url_crawling.c_str(), agent.c_str());
+        slong_int rddwebc_get_return = rddwebc_get(url_crawling, agent);
 
-        cout << "[***] URL REQUEST: " << url_crawling <<
-             "\n[***] HTTP RESPONSE STATUS CODE: [ " << rddwebc_get_return << " ] "<<
-              "\n[***] USER-AGENT: " << agent <<
-             "\n[***] REQUISITION DATE AND TIME: " << TIME_STAMP() << endl;
+        std::cout << "[***] URL REQUEST: " << url_crawling <<
+                     "\n[***] HTTP RESPONSE STATUS CODE: [ " << rddwebc_get_return << " ] " <<
+                     "\n[***] USER-AGENT: " << agent <<
+                     "\n[***] REQUISITION DATE AND TIME: " << time_stamp() << std::endl;
     }
 }
 
+
 int main(int argc, char *argv[])
 {
-    char const *url = "", *path_string_or_wordlist = "", *agent = "";
+    std::string url, path_string_or_wordlist, agent;
     int option_index, arguments_parse;
-    bool flag_url_empty = true;
 
     static const struct option long_options[] = {
         { "url",      required_argument,  NULL, 'u'},
@@ -210,7 +223,6 @@ int main(int argc, char *argv[])
         switch (arguments_parse) {
             case 'u':
                 url = optarg;
-                flag_url_empty = false;
                 break;
             case 'p':
                 path_string_or_wordlist = optarg;
@@ -219,20 +231,19 @@ int main(int argc, char *argv[])
                 agent = optarg;
                 break;
             default:
-                BANNER_RDDWEBC();
-                METHOD_USAGE_RDDWEBC();
+                banner_rddwebc();
+                method_usage_rddwebc();
                 exit(EXIT_FAILURE);
         }
     }
 
-    if (optind < 5 || flag_url_empty == true) {
-        BANNER_RDDWEBC();
-        METHOD_USAGE_RDDWEBC();
+    if (optind < 5 || url.empty()) {
+        banner_rddwebc();
+        method_usage_rddwebc();
         exit(EXIT_FAILURE);
     }
 
-    ___RDDWEBCRAWLING___(url, WORDLIST(path_string_or_wordlist), agent);
+    ___rddwebcrawling___(url, wordlist(path_string_or_wordlist), agent);
 
     return 0;
 }
-
